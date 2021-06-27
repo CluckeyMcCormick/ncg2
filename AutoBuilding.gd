65315a3c71968d4a98dc8c5ad0c2e203178401ba
frick-nedrickson <CluckeyMcCormick@github.com>
2021-06-27 15:31:01 -0700

    Generate a basic building
    Now that we've got some code for generating buildings, we need to do
    the actual generation. I'm taking a real slap-it-together approach,
    but I think it will work in the long run (with the caveat of some
    refinement).
    
    Firstly, the auto-building now generates a building by cell / window
    count, instead of by world-unit size. We do this using a UV_SIZE
    constant. Turns out, in Godot, the pixel-per-world-unit is
    1 / texture size. So I substituted the number of windows - 32 - to get
    the appropriate world-unit-per-window size.
    
    There does seem to be something off about the UV of the windows. When
    a side is odd, it needs to be shifted by half of a window. If it's
    even, it is fine.
    
    I also need to figure out a solution for the roofs. The ideal camera
    angle is actually going to be aiming upwards so IDEALLY we won't
    actually see the roofs. In the old neon-city generator this was
    handled by using vertex color - I wonder if it would be worth it to
    return to that instead of texture...

--- AutoBuilding.gd
+++ AutoBuilding.gd
@@ -1,6 +1,8 @@
+tool
 extends Spatial
 
-export(Material) var building_material
+export(Material) var building_side_material
+export(Material) var building_top_material
 
 var len_x = 3
 var len_y = 5
@@ -8,26 +10,54 @@ var len_z = 2
 
 # Load the PolyGen script
 const PolyGen = preload("res://util/PolyGen.gd")
+const WINDOW_UV_SIZE = 1.0 / 32.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     make_building()
 
 func make_building():
-    $ZNeg.mesh = ready_mesh(Vector2(0, 0), Vector2(1, 1), -1, false, Vector2(0, 0))
+    
+    var eff_x = (len_x * WINDOW_UV_SIZE) / 2
+    var eff_y = len_y * WINDOW_UV_SIZE
+    var eff_z = (len_z * WINDOW_UV_SIZE) / 2
+    
+    # By default, the UV2 texture is centered on 0,0 - so it stretches half it's
+    # length in either direction on x and z. While that normally wouldn't be
+    # a problem, with our pixel-perfect texture, it bisects one of the windows.
+    # So we need to shift it to the side by half of the size of a window.
+    var base_UV2_shift = Vector2(WINDOW_UV_SIZE / 2, 0)
+    
+    $ZNeg.mesh = ready_mesh_XZ(
+        Vector2(-eff_x, 0), 
+        Vector2( eff_x, eff_y), 
+        -eff_z, false, base_UV2_shift
+    )
+    $ZPos.mesh = ready_mesh_XZ( 
+        Vector2(-eff_x, eff_y),
+        Vector2( eff_x, 0), 
+        eff_z, false, base_UV2_shift
+    )
+    $XNeg.mesh = ready_mesh_XZ(
+        Vector2(-eff_z, 0), 
+        Vector2( eff_z, eff_y), 
+        eff_x, true, Vector2.ZERO
+    )
+    $XPos.mesh = ready_mesh_XZ( 
+        Vector2(-eff_z, eff_y),
+        Vector2( eff_z, 0), 
+        -eff_x, true, Vector2.ZERO
+    )
 
-func ready_mesh(pointA, pointB, axis_point, is_x, uv2_shift):    
+func ready_mesh_XZ(pointA, pointB, axis_point, is_x, uv2_shift):    
     var new_mesh = Mesh.new()
     var verts = PoolVector3Array()
     var UVs = PoolVector2Array()
     var UV2s = PoolVector2Array()
     
-    var length = 1
-    
     # Points and such
     var pd
     
-    # Face 1: X-Positive
     if is_x:
         pd = PolyGen.create_xlock_face_simple(pointA, pointB, axis_point, uv2_shift)
     else:
@@ -39,10 +69,42 @@ func ready_mesh(pointA, pointB, axis_point, is_x, uv2_shift):
     var st = SurfaceTool.new()
     st.begin(Mesh.PRIMITIVE_TRIANGLES)
     
-    st.set_material(building_material)
+    st.set_material(building_side_material)
 
     for v in verts.size():
         st.add_uv(UVs[v])
+        st.add_uv2(UV2s[v])
+        st.add_vertex(verts[v])
+
+    st.generate_normals()
+    st.generate_tangents()
+
+    st.commit(new_mesh)
+    return new_mesh
+
+func ready_mesh_Y(pointA, pointB, axis_point):    
+    var new_mesh = Mesh.new()
+    var verts = PoolVector3Array()
+    var UVs = PoolVector2Array()
+    var UV2s = PoolVector2Array()
+    
+    # Points and such
+    var pd
+    
+    pd = PolyGen.create_ylock_face_simple(pointA, pointB, axis_point, Vector2.ZERO)
+    
+    verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+    UVs.append_array( pd[PolyGen.UV_KEY] )
+    UV2s.append_array( pd[PolyGen.UV2_KEY] )
+    
+    var st = SurfaceTool.new()
+    st.begin(Mesh.PRIMITIVE_TRIANGLES)
+    
+    st.set_material(building_top_material)
+
+    for v in verts.size():
+        st.add_uv(UVs[v])
+        st.add_uv2(UV2s[v])
         st.add_vertex(verts[v])
 
     st.generate_normals()
