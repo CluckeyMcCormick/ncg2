681eb47eba116bdcee0edd5d3ac69651a2d853a5
frick-nedrickson <CluckeyMcCormick@github.com>
2021-07-01 22:37:40 -0700

    Randomize UV Shift for cells/windows per-face
    Previously, all of the building faces were using the world aligned UV
    values (save the odd-even correction). The problem was that meant
    every single face of the building was the same, except for some
    incidental mirroring.
    
    So, I've added a new function to calculate the cell-based UV shift
    with some random thrown in for good measure. I moved the odd-even
    correction into this function to help simplify everything visually.
    
    While the result works, I'm unsure how much I like it. I think the
    window texture is too small - we need a larger texture, which means
    we'll need a larger street fade texture. Though I'm growing less and
    less crazy about the dithering. The smooth colors of the original Neon
    City Generator was one of that project's greatest features. I need to
    try and recapture that somehow...

--- AutoBuilding.gd
+++ AutoBuilding.gd
@@ -3,12 +3,17 @@ extends Spatial
 
 # Load the PolyGen script
 const PolyGen = preload("res://util/PolyGen.gd")
+
+# We assume that the 'window texture' is made up of a square of cells. How many
+# cells make up one length of the image? This should be 32, but we're not going
+# to take that for granted.
+const WINDOW_CELL_LEN = 32
 # We measure each building by the number of 'cells' - in other words, the
 # windows. So we need to know how big, in world units, each cell is. Turns out,
 # Godot makes it so that each texture automatically sizes itself to one world
-# unit. Since our texture is 32 cells by 32 cells, the one world unit divided by
-# 32 gets us the measure of a cell in the world.
-const WINDOW_UV_SIZE = 1.0 / 32.0
+# unit. Since our texture SHOULD be 32 cells by 32 cells, one world unit divided
+# by WINDOW_CELL_LEN gets us the measure of a cell in the world.
+const WINDOW_UV_SIZE = 1.0 / WINDOW_CELL_LEN
 # Because we're using a texture for the top of the building, we have to pull
 # some special crap to "select" the texture depending on the height. What's the
 # max height a building can be before we default to the untextured top?
@@ -18,15 +23,23 @@ const MAX_TEXTURED_TOP_HEIGHT = 6
 # designate the "lower" building (inlusive)?
 const TOP_LOW_SPLIT = 4
 
+# By default, the UV2 texture is centered on 0,0 - so it stretches half it's
+# length in either direction on x and z. While that normally wouldn't be
+# a problem, with our pixel-perfect texture, it bisects one of the windows
+# IF the measure on that side is odd. So we need to shift it to the side by
+# half of the size of a window.
+const odd_UV2_shift = Vector2(WINDOW_UV_SIZE / 2, 0)
+
 export(Material) var building_side_material
 export(Material) var building_top_material
 
-export(int) var len_x = 3 setget set_length_x
-export(int) var len_y = 6 setget set_length_y
-export(int) var len_z = 2 setget set_length_z
+export(int) var len_x = 8 setget set_length_x
+export(int) var len_y = 16 setget set_length_y
+export(int) var len_z = 8 setget set_length_z
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    randomize()
     make_building()
 
 # --------------------------------------------------------
@@ -48,45 +61,62 @@ func set_length_z(new_length):
     len_z = new_length
     if Engine.editor_hint:
         make_building()
-     
 
 # --------------------------------------------------------
 #
 # Build Functions
 #
 # --------------------------------------------------------
+func calculate_uv_shift(side_len, random_add : bool = true):
+    # We'll stick the return value into this Vector2
+    var return_val
+    
+    if random_add:
+        return_val = Vector2(randi() % WINDOW_CELL_LEN, randi() % WINDOW_CELL_LEN)
+        return_val *= WINDOW_UV_SIZE
+    else:
+        return_val = Vector2.ZERO
+    
+    # Next, shift the Vector2 as appropriate for the input side length. Using
+    # the parity to scale the shift allows us to skip using an if/else.
+    return_val += (side_len % 2) * odd_UV2_shift
+    
+    print(return_val)
+    
+    return return_val
+
 func make_building():
+    
+    # If we don't have the building nodes for whatever reason, back out
+    if not (self.has_node("ZNeg") or self.has_node("ZPos") or \
+    self.has_node("XNeg") or self.has_node("XPos") ):
+        return
+    
+    print("good")
     
     var eff_x = (len_x * WINDOW_UV_SIZE) / 2
     var eff_y = len_y * WINDOW_UV_SIZE
     var eff_z = (len_z * WINDOW_UV_SIZE) / 2
     
-    # By default, the UV2 texture is centered on 0,0 - so it stretches half it's
-    # length in either direction on x and z. While that normally wouldn't be
-    # a problem, with our pixel-perfect texture, it bisects one of the windows
-    # IF the measure on that side is odd. So we need to shift it to the side by
-    # half of the size of a window.
-    var odd_UV2_shift = Vector2(WINDOW_UV_SIZE / 2, 0)
-    
     $ZNeg.mesh = ready_side_mesh(
         Vector2(-eff_x, 0), 
         Vector2( eff_x, eff_y), 
-        -eff_z, false, (len_x % 2) * odd_UV2_shift
+        -eff_z, false, calculate_uv_shift(len_x)
     )
     $ZPos.mesh = ready_side_mesh( 
         Vector2(-eff_x, eff_y),
         Vector2( eff_x, 0), 
-        eff_z, false, (len_x % 2) * odd_UV2_shift
+        eff_z, false, calculate_uv_shift(len_x)
     )
     $XNeg.mesh = ready_side_mesh(
         Vector2(-eff_z, 0), 
         Vector2( eff_z, eff_y), 
-        eff_x, true, (len_z % 2) * odd_UV2_shift
+        eff_x, true, calculate_uv_shift(len_z)
     )
     $XPos.mesh = ready_side_mesh( 
         Vector2(-eff_z, eff_y),
-        Vector2( eff_z, 0), 
-        -eff_x, true, (len_z % 2) * odd_UV2_shift
+        Vector2( eff_z, 0),
+        -eff_x, true, calculate_uv_shift(len_z)
     )
     $YTop.mesh = ready_top_mesh(
         Vector2(-eff_x, -eff_z),
