3961a111a16465a90bca857e4c9eb56010d3aecb
frick-nedrickson <CluckeyMcCormick@github.com>
2021-07-05 12:45:10 -0700

    Clean up the sleds once they exit the screen
    In order to make things nice and clean, we're gonna have the sleds
    clean themselves up once they exit the screen. So they're gonna be
    basically autonomous - what we need now is something to handle
    spawning them...

--- scrollers/ScrollSled.gd
+++ scrollers/ScrollSled.gd
@@ -9,6 +9,9 @@ export(int) var slide_depth = 2 setget set_slide_depth
 # How fast does the sled scroll, in units/sec?
 export(int) var move_speed = 2
 
+# Is the sled's building visible?
+var building_vis = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     make_slide()
@@ -19,7 +22,7 @@ func _process(delta):
 
 # --------------------------------------------------------
 #
-# Setters and Getters
+# Setters, Getters, Signals
 #
 # --------------------------------------------------------
 func set_slide_depth(new_depth):
@@ -30,6 +33,12 @@ func set_slide_depth(new_depth):
     if self.has_node("OmniLight"):
         # Move the Omni-Light, as appropriate.
         $OmniLight.translation.z = new_depth / 2
+
+func _on_VisibilityNotifier_screen_exited():
+    # We exited the screen! This only happens on an exit, meaning we've already
+    # entered the screen before. That means our scroll is done, and it's time to
+    # delete the sled.
+    queue_free()
 
 # --------------------------------------------------------
 #
