36113c021c069e8da4640db7a7dc2aa1dfff0b5b
frick-nedrickson <CluckeyMcCormick@github.com>
2021-07-05 19:42:19 -0700

    Spawn and organize sleds using lanes
    With the scroll sled, we have an effective way of spawning a building
    and a light, and then making them scroll together. A good way of
    bundling things.
    
    But we needed something to manage those bundles - and that's where the
    SledLanes came in. The lane objects control the random aspects of
    their respective sleds. They handle spawning the sleds, ensuring they
    are appropriately spaced out and randomized within the lane's
    variables.
    
    This necessitated changes to the ScrollSled scene - specifically
    because the SledLanes needed to modify the ScrollSled's building and
    other such elements. We'll probably modify this more as time goes
    on...

--- scrollers/ScrollSled.gd
+++ scrollers/ScrollSled.gd
@@ -3,6 +3,8 @@ extends Spatial
 # Load the GlobalRef script
 const GlobalRef = preload("res://util/GlobalRef.gd")
 
+const FSB_SHIFT = GlobalRef.WINDOW_UV_SIZE * 2
+
 # How "deep" (length on z) is the slide as a whole?
 export(int) var slide_depth = 2 setget set_slide_depth
 
@@ -11,6 +13,12 @@ export(int) var move_speed = 2
 
 # Is the sled's building visible?
 var building_vis = false
+
+# What we consider to be the effective length of the sled, for reference. This
+# isn't actually an exact calculation, especially if the building has been
+# rotated; rather, it reflects the maximum possible extent of space that a
+# building can take up. This is set when the sled is built; 1 is just a default.
+var max_poss_x_len = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,6 +42,14 @@ func set_slide_depth(new_depth):
         # Move the Omni-Light, as appropriate.
         $OmniLight.translation.z = new_depth / 2
 
+# A wrapper function to set the proportions of the building, since whatever
+# spawns the sled might not be able to reach down directly.
+func set_building_size(len_x, len_z, base_height, tower_height):
+    $FSB.len_x = len_x
+    $FSB.len_z = len_z
+    $FSB.base_len_y = base_height
+    $FSB.tower_len_y = tower_height
+
 func _on_VisibilityNotifier_screen_exited():
     # We exited the screen! This only happens on an exit, meaning we've already
     # entered the screen before. That means our scroll is done, and it's time to
@@ -46,13 +62,22 @@ func _on_VisibilityNotifier_screen_exited():
 #
 # --------------------------------------------------------
 func make_slide():
-    # We'll assume the 
     # Move the Omni-Light, as appropriate.
     $OmniLight.translation.z = slide_depth / 2
     
+    # Calculate the max 
     var length = ($FSB.len_x * GlobalRef.WINDOW_UV_SIZE) * $FSB.scale.x
     var depth = ($FSB.len_z * GlobalRef.WINDOW_UV_SIZE) * $FSB.scale.z
     var c_squared = sqrt(pow(length, 2) + pow(depth, 2))
     var maxed = max( max(length, depth), c_squared )
     
-    $FSB.translation.x = (maxed / 2) + 1
+    # Translate the building over by the appropriate shift AND half the
+    # max-possible size of the sled.
+    $FSB.translation.x = (FSB_SHIFT * $FSB.scale.x) + (maxed / 2)
+    # Set that max-possible length on x
+    max_poss_x_len = (FSB_SHIFT * $FSB.scale.x) + maxed
+
+# A wrapper function to make the building, since whatever spawns the sled might
+# not be able to reach down directly.
+func make_building():
+    $FSB.make_building()
---
+++ scrollers/SledLane.gd
@@ -0,0 +1,69 @@
+extends Spatial
+
+# Load the scroll-sled scene
+const scroll_sled_scene = preload("res://scrollers/ScrollSled.tscn")
+
+# What's the minimum length for a building in this lane on x?
+export(int) var width_min = 5
+# What's the maximum length for a building in this lane on x?
+export(int) var width_max = 9
+
+# What's the minimum length for a building in this lane on z?
+export(int) var depth_min = 4
+# What's the maximum length for a building in this lane on z?
+export(int) var depth_max = 8
+
+# What's the minimum length for a building's base, in this lane, on y?
+export(int) var base_height_min = 2
+# What's the maximum length for a building's base, in this lane, on y?
+export(int) var base_height_max = 4
+
+# What's the minimum length for a building in this lane on y?
+export(int) var tower_height_min = 4
+# What's the maximum length for a building in this lane on y?
+export(int) var tower_height_max = 8
+
+# What's the last sled we made?
+var last_sled = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    make_sled()
+
+func _process(delta):
+    # If we don't have a last sled, back out.
+    if last_sled == null:
+        return
+    
+    # Otherwise, if the local translation of the sled, on X, exceeds or meets
+    # the potential max x-length of the sled...
+    if abs(last_sled.translation.x) >= last_sled.max_poss_x_len:
+        # Then make a new sled
+        make_sled()
+
+# ~~~~~~~~~~~~~
+
+func get_random_int(min_val, max_val):
+    return (randi() % (max_val - min_val)) + min_val
+
+func make_sled():
+    # Create a new sled
+    var new_sled = scroll_sled_scene.instance()
+    
+    # Roll the different lengths
+    var len_x = get_random_int(width_min, width_max)
+    var len_z = get_random_int(depth_min, depth_max)
+    var base_height = get_random_int(base_height_min, base_height_max)
+    var tower_height = get_random_int(tower_height_min, tower_height_max)
+    
+    # Pass those different lengths down to the FSB
+    new_sled.set_building_size(len_x, len_z, base_height, tower_height)
+    
+    # Stick the sled in the tree
+    self.add_child(new_sled)
+    # Force the sled's FSB to build
+    new_sled.make_building()
+    
+    # This is now the last sled 
+    last_sled = new_sled
+
