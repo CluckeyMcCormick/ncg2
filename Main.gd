7348148808fe8dc2c7e2f707972a4b1c5ee2860b
frick-nedrickson <CluckeyMcCormick@github.com>
2021-07-10 12:41:40 -0700

    Add a GUI to live-change the different lights
    The ultimate end-goal of this project is to have it only PARTIALLY
    dependent on the Godot IDE. I want the user to have some basic metric
    of control.
    
    To start, I've added a GUI to allow the user to change our three light
    colors on the fly. The new colors slowly trickle in with new
    buildings.
    
    I originally considered having the lights change in real time. This
    would be feasible with Godot's Group functionality but I was worried
    about performance impacts and decided not to.

--- Main.gd
+++ Main.gd
@@ -1,16 +1,26 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# Grab the MaterialColorControl Node - this will allow us to change colors and
+# materials on the fly.
+onready var mcc = get_node("/root/MaterialColorControl")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     randomize()
+    
+    $GUI/TabContainer/Lights/VBoxContainer/ColorPickerButton1.color = mcc.light_color_one
+    $GUI/TabContainer/Lights/VBoxContainer/ColorPickerButton2.color = mcc.light_color_two
+    $GUI/TabContainer/Lights/VBoxContainer/ColorPickerButton3.color = mcc.light_color_three
 
+func _input(event):
+    if event.is_action_pressed("gui_toggle"):
+        $GUI.visible = not $GUI.visible
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _on_ColorPickerButton1_color_changed(color):
+    mcc.light_color_one = color
+
+func _on_ColorPickerButton2_color_changed(color):
+    mcc.light_color_two = color
+
+func _on_ColorPickerButton3_color_changed(color):
+    mcc.light_color_three = color
