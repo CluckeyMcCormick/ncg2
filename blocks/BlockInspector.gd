ba4e29ed1313b1a182bf8380c4526bc63224a0d7
frick-nedrickson <CluckeyMcCormick@github.com>
2021-09-19 22:05:49 -0700

    Randomize blocks via manual UV 'derivatives'
    Okay, so I wasn't able to adjust the UV faces like I wanted to via
    Qodot because Qodot just builds whatever is in the .map file.
    
    Then I realized something: the .map file is written in a very
    particular way. It's deliberately written for easy machine processing,
    with the values separated by spaces and presented in a very specific
    order. And, I was able to spot which two values represented our UV
    shift!
    
    Since Qodot allows us to build at run time, we just need to modify a
    file at run time to create a derivative, and then feed it into our
    QodotMap node. Presto! A UV shifted building!
    
    Our current technique is to just take in a file and automatically
    write a 'derivative' version. We generate the derivative by
    processing only those lines that match our building texture. In the
    future we'll have stricter control over the randomization, and we'll
    probably use some sort of 'caching' to avoid generating unecessary
    files.

--- blocks/BlockInspector.gd
+++ blocks/BlockInspector.gd
@@ -13,3 +13,44 @@ func _ready():
         Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
     )
     $CameraPivot/Tween.start()
+    
+    randomize()
+    
+    var new_map = make_derivative("res://blocks/trenchbroom_maps/block01.map")
+    print(ProjectSettings.globalize_path(new_map))
+    $QodotMap.map_file = ProjectSettings.globalize_path(new_map)
+    $QodotMap.verify_and_build()
+    
+func make_derivative(map_path):
+    var target_path = "user://blk_drv_"
+    target_path += map_path.get_file().get_basename() + "_01"
+    target_path += "." + map_path.get_extension()
+    
+    var in_map = File.new()
+    in_map.open(map_path, File.READ)
+    var out_map = File.new()
+    out_map.open(target_path, File.WRITE)
+    
+    while not in_map.eof_reached():
+        var line = in_map.get_line()
+        var keyword_index = line.find("building_horizontal_window")
+        
+        if keyword_index == -1:
+            out_map.store_line(line)
+            continue
+        
+        var new_line = line.substr(0, keyword_index)
+        var old_line_comp = line.substr(keyword_index).split(" ", false)
+        
+        new_line += old_line_comp[0]
+        new_line += " " + str((randi() % 64) * 64)
+        new_line += " " + str((randi() % 64) * 64)
+        new_line += " " + old_line_comp[3]
+        new_line += " " + old_line_comp[4]
+        new_line += " " + old_line_comp[5]
+        out_map.store_line(new_line)
+
+    in_map.close()
+    out_map.close()
+    
+    return target_path
