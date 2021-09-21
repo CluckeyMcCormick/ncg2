076cd70ec4781dd36190b680915b36f9efbcdaed
frick-nedrickson <CluckeyMcCormick@github.com>
2021-09-20 20:09:04 -0700

    Upgrade BlockInspector with dyanmic block menu
    Previously, the BlockInspector was hard coded to load only a
    particular map. For the whole of my Godot experience, this has been
    how it had to be. I was always vaguely aware that there was PROBABLY
    a better option but, to be quite frank, I'd never really been bothered
    by it.
    
    Of course, as we started messing with these dynamic-derivative files,
    my eyes opened to the possibility that we could dynamically take stock
    of files that we do or don't have.
    
    So, I added a GUI to the BlockInspector. The GUI has an options menu
    that is dynamically populated with all of the .map files in the
    trenchbroom_maps directory. Pressing the "Go" button will trigger a
    new derivative of the selected map to be generated and loaded.
    
    To test this out further, I added two new blocks. Looks good so far!
    
    One odd thing I noticed was that rotating the buildings in TrenchBroom
    resulted in ever-so-slightly offset textures. This was resulting in
    "bad" windows because we completely ignored the .map file's existing
    offset. To counter, we now add in the existing offset to our
    derivative offset.

--- blocks/BlockInspector.gd
+++ blocks/BlockInspector.gd
@@ -1,30 +1,64 @@
 extends Spatial
 
+const MAP_PATH = "res://blocks/trenchbroom_maps/"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var BLOCK_CHOICE_NODE = $GUI/Panel/HBox/VBox/BlockChoiceButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    # First, start the camera's eternally circular journey by telling it to
+    # always rotate the CameraPivot node 360 degrees.
     $CameraPivot/Tween.interpolate_property(
         $CameraPivot, "rotation_degrees:y", 0, 360, 5,
         Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
     )
+    # Start it up!
     $CameraPivot/Tween.start()
     
-    randomize()
+    # Now we need to dynamically build up our list of blocks we can use. To do
+    # so, we're gonna take a look at the blocks directory and see what we can
+    # do.
+    var dir = Directory.new()
+    # If the directory didn't open, then we can't really do anything at all.
+    # Ergo, we'll have to back out.
+    if dir.open(MAP_PATH) != OK:
+        print("Couldn't load TrenchBroom Maps directory! Can't do anything!!!")
+        return
     
-    var new_map = make_derivative("res://blocks/trenchbroom_maps/block01.map")
-    print(ProjectSettings.globalize_path(new_map))
+    # Start the directory-listing-processing thing.
+    dir.list_dir_begin(true)
+    
+    # Get the first file in the directory listing
+    var file_name = dir.get_next()
+    # While we still have a file...
+    while file_name != "":
+        # If the current file is NOT a directory...
+        if (not dir.current_is_dir()):
+            # AND it is a .map file but is NOT a .import file...
+            if ".map" in file_name and not (".import" in file_name):
+                # Then add it to the choice list!
+                BLOCK_CHOICE_NODE.add_item(file_name)
+        # Now that we've checked the filename and done any necessary actions,
+        # get the next file name.
+        file_name = dir.get_next()
+    
+    # Finish off the directory listing processing... thing.
+    dir.list_dir_end()
+    
+    # Now, get the ID of the selected map. This should be the first one, by
+    # default.
+    var map_id = BLOCK_CHOICE_NODE.get_selected_id()
+    # Now, make a derivative map file using that block-map.
+    var new_map = make_derivative(MAP_PATH + BLOCK_CHOICE_NODE.get_item_text(map_id))
+    
+    # Finally, pass that derivative map into Qodot and rebuild.
     $QodotMap.map_file = ProjectSettings.globalize_path(new_map)
     $QodotMap.verify_and_build()
     
 func make_derivative(map_path):
-    var target_path = "user://blk_drv_"
-    target_path += map_path.get_file().get_basename() + "_01"
-    target_path += "." + map_path.get_extension()
+    randomize()
+    
+    var target_path = "user://block_viewer_cache.map"
     
     var in_map = File.new()
     in_map.open(map_path, File.READ)
@@ -43,8 +77,8 @@ func make_derivative(map_path):
         var old_line_comp = line.substr(keyword_index).split(" ", false)
         
         new_line += old_line_comp[0]
-        new_line += " " + str((randi() % 64) * 64)
-        new_line += " " + str((randi() % 64) * 64)
+        new_line += " " + str((randi() % 64) * 64 + float(old_line_comp[1]))
+        new_line += " " + str((randi() % 64) * 64 + float(old_line_comp[2]))
         new_line += " " + old_line_comp[3]
         new_line += " " + old_line_comp[4]
         new_line += " " + old_line_comp[5]
@@ -54,3 +88,15 @@ func make_derivative(map_path):
     out_map.close()
     
     return target_path
+
+func _on_GoButton_pressed():
+    # Get the ID of the selected map. This should be the first one, by
+    # default.
+    var map_id = BLOCK_CHOICE_NODE.get_selected_id()
+    # Now, make a derivative map file using that block-map.
+    print(BLOCK_CHOICE_NODE.items[map_id])
+    var new_map = make_derivative(MAP_PATH + BLOCK_CHOICE_NODE.get_item_text(map_id))
+    
+    # Finally, pass that derivative map into Qodot and rebuild.
+    $QodotMap.map_file = ProjectSettings.globalize_path(new_map)
+    $QodotMap.verify_and_build()
