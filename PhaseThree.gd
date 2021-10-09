255f6c7599d323bd0b561b72af20c8e3395acf54
frick-nedrickson <CluckeyMcCormick@github.com>
2021-10-09 14:59:02 -0700

    Load blocks incrementally via coroutines
    Alright so as we established in the LAST commit, you can't load in all
    the Qodot blocks at once. It breaks everything.
    
    Next best thing: load them all in one-at-a-time with a fixed duration
    between the loads. So, I created a distinct scene to handle the
    loading: the BlockCache. It builds everything for us - the only trick
    is that, by default, the build_all_blocks function yields after every
    block.
    
    Thus, it's up to whatever scene is actively managing the BlockCache to
    handle when to continue building the blocks. Currently that's JUST the
    Phase Three scene. I found we could get a pretty crashless result if
    we waited one second before starting the build and then .35 seconds
    between block builds.
    
    I'm genuinely worried that those times may be platform or even
    hardware specific (perhaps a quirk of Godot?) but I am woefully bereft
    of any additional hardware to test this theory. Maybe I can trick my
    friends into helping me?

--- PhaseThree.gd
+++ PhaseThree.gd
@@ -1,140 +1,39 @@
 extends Spatial
 
-# Where do we look for maps?
-const IN_MAP_PATH = "res://blocks/trenchbroom_maps/"
-# Where do we write the derivative maps to?
-const OUT_MAP_PATH = "user://"
-# How many derivatives do we make for each map?
-const MAP_DERIVATIVE_COUNT = 5
+# The co-routine return value. We use this to build up all the blocks
+# progressively
+var co_ro
 
-# What's the inverse scale factor do we use when making the Qodot blocks?
-const BLOCK_INVERSE_SCALE = 128
-# How big is the side of one block, in Qodot units?
-const QODOT_BLOCK_SIDE_LENGTH = 1024
-# Using the last two constants, what is our Godot unit side-length?
-const BLOCK_SIDE_LENGTH = QODOT_BLOCK_SIDE_LENGTH / BLOCK_INVERSE_SCALE
+# Capture the shifted position of the orthographic camera once we enter the
+# scene. That way we can shift it around but mantain it's position
+onready var ortho_shift = $OrthoCamera.translation
 
-# What are the QodotMap nodes we're going to look at?
-var map_nodes = []
+# Emitted when we finish building the cache.
+signal cache_build_finished()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    
-    print("run")
-    
-    # Create a blank file pointer - we'll need this later to test whether files
-    # exist
-    var file = File.new()
-    
-    # Now we need to dynamically build up our list of blocks we can use. To do
-    # so, we're gonna take a look at the blocks directory and see what we can
-    # do.
-    var dir = Directory.new()
-    # If the directory didn't open, then we can't really do anything at all.
-    # Ergo, we'll have to back out.
-    if dir.open(IN_MAP_PATH) != OK:
-        print("Couldn't load TrenchBroom Maps directory! Can't do anything!!!")
-        return
-    
-    # Start the directory-listing-processing thing.
-    dir.list_dir_begin(true)
-    
-    # Randomize so we get some PROPER RANDOM SHIFTING
-    randomize()
-    
-    var z_shift = 0
-    
-    # Get the first file in the directory listing
-    var file_name = dir.get_next()
-    # While we still have a file...
-    while file_name != "":        
-        # If the current file is a directory, get the next filename and skip!
-        if dir.current_is_dir():
-            file_name = dir.get_next()
-            continue
-        # If it's not a map, get the next filename and skip!
-        if not ".map" in file_name:
-            file_name = dir.get_next()
-            continue
-        # If it's an import, get the next filename and skip!
-        if ".import" in file_name:
-            file_name = dir.get_next()
-            continue
-        
-        # Okay, we have a valid map. Now we need to make derivatives. First,
-        # let's break the map up into it's constituent parts:
-        var only_name = file_name.get_file().get_basename()
-        var extension = file_name.get_extension()
-        
-        # Okay, now we need to make derivatives for each.
-        for i in range(MAP_DERIVATIVE_COUNT):
-            # Cook up this derivative's name
-            var cur_deriv = OUT_MAP_PATH + only_name
-            cur_deriv += "_d" + str(i + 1)
-            cur_deriv += "." + extension
-            
-            # If the file doesn't exist, then we'll MAKE IT EXIST!!!
-            if not file.file_exists(cur_deriv):
-                make_single_derivative(IN_MAP_PATH + file_name, cur_deriv)
-            
-            # Okay, now we need to make a QodotMap Node for this derivative.
-            var new_qodot = QodotMap.new()
-            # Stick it under the "cached maps" section
-            $CachedMaps.add_child(new_qodot)
-            # Also stick it in our list of map nodes
-            map_nodes.append(new_qodot)
-            
-            # Shift the qodot map
-            new_qodot.translation.x = i * BLOCK_SIDE_LENGTH
-            new_qodot.translation.z = z_shift
-            
-            # Update the Inverse Scale Factor
-            new_qodot.inverse_scale_factor = BLOCK_INVERSE_SCALE
-            
-            # Give it the path to the current derivative map
-            new_qodot.map_file = ProjectSettings.globalize_path(cur_deriv)
-            
-            # BUILD IT!!!
-            new_qodot.verify_and_build()
-        
-        # That's done! Increment our z_shift by the appropriate factor
-        z_shift += BLOCK_SIDE_LENGTH
-        
-        if z_shift >= 8:
-            break
-        
-        # Now that we've checked the filename and done any necessary actions,
-        # get the next file name.
-        file_name = dir.get_next()
-    
-    # Finish off the directory listing processing... thing.
-    dir.list_dir_end()
+    # Give Godot a second to get started. Building the blocks too fast breaks
+    # everything for some reason.
+    yield(get_tree().create_timer(1.0), "timeout")
+    # Start building all of the blocks, capture the co-routine yield
+    co_ro = $BlockCache.build_all_blocks()
+    # Start the Build Timer. We need the timer because building Qodot maps too
+    # quickly breaks everything.
+    $BlockBuildTimer.start()
 
-func make_single_derivative(in_path, out_path):
-    
-    var in_map = File.new()
-    in_map.open(in_path, File.READ)
-    var out_map = File.new()
-    out_map.open(out_path, File.WRITE)
-    
-    while not in_map.eof_reached():
-        var line = in_map.get_line()
-        var keyword_index = line.find("building_horizontal_window")
+# Whenever we timeout, build resume the BlockCache build process.
+func _on_BlockBuildTimer_timeout():
+    # If the co-routine is active...
+    if co_ro is GDScriptFunctionState && co_ro.is_valid():
+        # Call the co-routine.
+        co_ro = co_ro.resume();
         
-        if keyword_index == -1:
-            out_map.store_line(line)
-            continue
-        
-        var new_line = line.substr(0, keyword_index)
-        var old_line_comp = line.substr(keyword_index).split(" ", false)
-        
-        new_line += old_line_comp[0]
-        new_line += " " + str((randi() % 64) * 64 + float(old_line_comp[1]))
-        new_line += " " + str((randi() % 64) * 64 + float(old_line_comp[2]))
-        new_line += " " + old_line_comp[3]
-        new_line += " " + old_line_comp[4]
-        new_line += " " + old_line_comp[5]
-        out_map.store_line(new_line)
+        # If the co-routine is still active...
+        if co_ro is GDScriptFunctionState:
+            # Schedule the BlockBuildTimer. Again.
+            $BlockBuildTimer.start()
 
-    in_map.close()
-    out_map.close()
+# Whenever the BlockCache builds a block, update the Camera position.
+func _on_BlockCache_block_built(godot_path, real_path, local_pos, global_pos):
+    $OrthoCamera.global_transform.origin = global_pos + ortho_shift
---
+++ blocks/BlockCache.gd
@@ -0,0 +1,174 @@
+extends Spatial
+
+# Where do we look for maps?
+const IN_MAP_PATH = "res://blocks/trenchbroom_maps/"
+# Where do we write the derivative maps to?
+const OUT_MAP_PATH = "user://"
+# How many derivatives do we make for each map?
+const MAP_DERIVATIVE_COUNT = 5
+# What's the inverse scale factor do we use when making the Qodot blocks?
+const BLOCK_INVERSE_SCALE = 128
+# How big is the side of one block, in Qodot units?
+const QODOT_BLOCK_SIDE_LENGTH = 1024
+# Using the last two constants, what is our Godot unit side-length?
+const BLOCK_SIDE_LENGTH = QODOT_BLOCK_SIDE_LENGTH / BLOCK_INVERSE_SCALE
+
+# When caching the blocks, we spawn them in real-space - how many blocks do we
+# do for a row?
+const BLOCKS_PER_ROW = 10
+
+# WHAT VALUE DO WE RETURN WHEN the Build All Blocks function succeeds?
+const BUILD_ALL_SUCCESS = 0
+
+# Here's where we'll stick the QodotMaps once we've built them - it what we'll
+# use to quickly get info from the node.
+var map_to_node = {}
+
+# Fires every time a block is built. Gives the godot file path (i.e. user:// or
+# res://), the real file path (i.e. /home/developer/...), as well as the local
+# and global positions. Not really that necessary, but useful for debuggery and
+# similar situations.
+signal block_built(godot_path, real_path, local_pos, global_pos)
+
+func build_all_blocks(yield_after_build=true):
+    # Create a blank file pointer - we need this to test whether files exist or
+    # not.
+    var existence_checker = File.new()
+
+    # Now - we need to dynamically build up our list of blocks we can use. To do
+    # so, we're gonna take a look at the blocks directory and see what we can
+    # do.
+    var dir = Directory.new()
+    
+    # If the directory didn't open, then we can't really do anything at all.
+    # Ergo, we'll have to back out.
+    if dir.open(IN_MAP_PATH) != OK:
+        printerr("Couldn't load TrenchBroom Maps directory! Can't do anything!!!")
+        return ERR_FILE_NOT_FOUND
+    
+    # Start the directory-listing-processing thing.
+    dir.list_dir_begin(true)
+    
+    # Randomize so we get some PROPER RANDOM SHIFTING
+    randomize()
+    
+    # Get the first file in the directory listing
+    var file_name = dir.get_next()
+    # While we still have a file...
+    while file_name != "":        
+        # If the current file is a directory, get the next filename and skip!
+        if dir.current_is_dir():
+            file_name = dir.get_next()
+            continue
+        # If it's not a map, get the next filename and skip!
+        if not ".map" in file_name:
+            file_name = dir.get_next()
+            continue
+        # If it's an import, get the next filename and skip!
+        if ".import" in file_name:
+            file_name = dir.get_next()
+            continue
+        
+        # Okay, we have a valid map. Now we need to make derivatives. First,
+        # let's break the map up into it's constituent parts:
+        var only_name = file_name.get_file().get_basename()
+        var extension = file_name.get_extension()
+        
+        # Okay, now we need to make derivatives for each.
+        for i in range(MAP_DERIVATIVE_COUNT):
+            # Cook up this derivative's name
+            var cur_deriv = OUT_MAP_PATH + only_name
+            cur_deriv += "_d" + str(i + 1)
+            cur_deriv += "." + extension
+            
+            # If the file doesn't exist, then we'll MAKE IT EXIST!!!
+            if not existence_checker.file_exists(cur_deriv):
+                make_single_derivative(IN_MAP_PATH + file_name, cur_deriv)
+            # Okay, the file exists now. Goody! Let's build it.
+            build_single_block(cur_deriv)
+            
+            # If we're yielding everytime we build something, then yield!!!
+            if yield_after_build:
+                yield();
+            
+        # Now that we've made the derivatives and built the block, get the next
+        # block-map-file.
+        file_name = dir.get_next()
+    
+    # Finish off the directory listing processing... thing.
+    dir.list_dir_end()
+    
+    # Return 0
+    return 0
+            
+func build_single_block(in_path):
+    # Create a blank file pointer - we need this to test whether files exist or
+    # not.
+    var existence_checker = File.new()
+    
+    # If the file doesn't exist, then throw out an error message and back out!
+    if not existence_checker.file_exists(in_path):
+        printerr("File ", in_path, " does not exist, cannot build!!!")
+        return
+    
+    # Okay so the file does exist. Yipee!!! Now, if we don't have a node
+    # associated with this map-path...
+    if not in_path in map_to_node:
+        # Okay, now we need to make a QodotMap Node.
+        var new_qodot = QodotMap.new()
+        # Stick it in the scene
+        self.add_child(new_qodot)
+            
+        # Shift the qodot map
+        new_qodot.translation.x = (len(map_to_node) % BLOCKS_PER_ROW) * BLOCK_SIDE_LENGTH
+        new_qodot.translation.z = (len(map_to_node) / BLOCKS_PER_ROW) * BLOCK_SIDE_LENGTH
+            
+        # Update the Inverse Scale Factor
+        new_qodot.inverse_scale_factor = BLOCK_INVERSE_SCALE
+        
+        # Give it the path to the map
+        new_qodot.map_file = ProjectSettings.globalize_path(in_path)
+        
+        # Finally, stick it in the dictionary
+        map_to_node[in_path] = new_qodot
+
+    # Okay, so the QodotMap node definitely exists. Time to build that sucker!
+    map_to_node[in_path].verify_and_build()
+    
+    # Tell anyone who's listening that we finished building a block. 
+    emit_signal(
+        "block_built", # signal type/name
+        in_path, # Godot Path
+        map_to_node[in_path].map_file, # Real Path
+        map_to_node[in_path].translation, # Local Position
+        map_to_node[in_path].global_transform.origin # Global Position.
+    )
+    
+func make_single_derivative(in_path, out_path):
+    
+    var in_map = File.new()
+    in_map.open(in_path, File.READ)
+    var out_map = File.new()
+    out_map.open(out_path, File.WRITE)
+    
+    while not in_map.eof_reached():
+        var line = in_map.get_line()
+        var keyword_index = line.find("building_horizontal_window")
+        
+        if keyword_index == -1:
+            out_map.store_line(line)
+            continue
+        
+        var new_line = line.substr(0, keyword_index)
+        var old_line_comp = line.substr(keyword_index).split(" ", false)
+        
+        new_line += old_line_comp[0]
+        new_line += " " + str((randi() % 64) * 64 + float(old_line_comp[1]))
+        new_line += " " + str((randi() % 64) * 64 + float(old_line_comp[2]))
+        new_line += " " + old_line_comp[3]
+        new_line += " " + old_line_comp[4]
+        new_line += " " + old_line_comp[5]
+        out_map.store_line(new_line)
+
+    in_map.close()
+    out_map.close()
