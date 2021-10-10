b3e015ae9445ea426ba7df4f4e196c6c3d2a50a9
frick-nedrickson <CluckeyMcCormick@github.com>
2021-10-09 17:17:36 -0700

    Manage PhaseThree better using a state machine
    Since I can't just shove all the initialization stuff in the ready
    function, I realized we were going to need some pretty complex logic
    to handle what needs to happen where and when.
    
    I was sort of bouncing around this idea of multiple scenes that passed
    around common data (like the BlockCache) when I remembered any
    complex-logic's best friend: The State Machine!
    
    After perusing the options, I went with imjp94's Yet Another Finite
    State Machine (YAFSM). The simplicity matched up with the needs of
    this project (it's not my insanity RTS, after all).
    
    I then adjusted the PhaseThree scene to utilize the state machine.
    Everything worked well, save for a few caveats: I had to maintain the
    one-second pause when we first enter the scene, AND I had to increase
    the block-build-gap to .5 seconds. I'm not sure what caused that gap
    increase, to be honest. It just started crashing on its own!

--- PhaseThree.gd
+++ PhaseThree.gd
@@ -8,22 +8,61 @@ var co_ro
 # scene. That way we can shift it around but mantain it's position
 onready var ortho_shift = $OrthoCamera.translation
 
-# Emitted when we finish building the cache.
-signal cache_build_finished()
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    # Give Godot a second to get started. Building the blocks too fast breaks
-    # everything for some reason.
     yield(get_tree().create_timer(1.0), "timeout")
-    # Start building all of the blocks, capture the co-routine yield
-    co_ro = $BlockCache.build_all_blocks()
-    # Start the Build Timer. We need the timer because building Qodot maps too
-    # quickly breaks everything.
-    $BlockBuildTimer.start()
+
+func _on_StateMachinePlayer_transited(from, to):
+    print("Transition: ", from, " |to| ", to)
+    
+    # First, handle any cleanup.
+    match from:
+        "Entry":
+            yield(get_tree().create_timer(1.0), "timeout")
+        
+        "CacheBuild":
+            # Connect the BlockBuildTimer and BlockCache signals for the
+            # CacheBuild state
+            $BlockBuildTimer.disconnect("timeout", self, "_CacheBuild_on_BlockBuildTimer_timeout")
+            $BlockBuildTimer.disconnect("timeout", self, "_CacheBuild_on_BlockCache_block_built")
+        
+            # Flip over the BlockCache
+            $BlockCache.rotation_degrees.x = -180
+            # Move the BlockCache down by one
+            $BlockCache.translation.y = -1
+            # Reset the OrthoCamera
+            $OrthoCamera.global_transform.origin = ortho_shift
+        _:
+            pass
+    
+    # Next, handle entry into the state
+    match to:
+        "CacheBuild":
+            # Connect the BlockBuildTimer and BlockCache signals for the
+            # CacheBuild state
+            $BlockBuildTimer.connect("timeout", self, "_CacheBuild_on_BlockBuildTimer_timeout")
+            $BlockCache.connect("block_built", self, "_CacheBuild_on_BlockCache_block_built")
+            
+            # Set the Orthogonal Camera to be the "current" camera.
+            $OrthoCamera.current = true
+            $PerspCamera.current = false
+            
+            # Start building all of the blocks, capture the co-routine yield
+            co_ro = $BlockCache.build_all_blocks()
+            
+            # Start the Build Timer. We need the timer because building Qodot
+            # maps too quickly breaks everything.
+            $BlockBuildTimer.start()
+            
+        "PerspectiveCity":
+            pass
+        "OrthogonalCity":
+            pass
+        _:
+            pass
+    pass # Replace with function body.
 
 # Whenever we timeout, build resume the BlockCache build process.
-func _on_BlockBuildTimer_timeout():
+func _CacheBuild_on_BlockBuildTimer_timeout():
     # If the co-routine is active...
     if co_ro is GDScriptFunctionState && co_ro.is_valid():
         # Call the co-routine.
@@ -33,7 +72,11 @@ func _on_BlockBuildTimer_timeout():
         if co_ro is GDScriptFunctionState:
             # Schedule the BlockBuildTimer. Again.
             $BlockBuildTimer.start()
+        # Otherwise...
+        else:
+            # Guess we're done! TRIGGER the state machine switch
+            $StateMachinePlayer.set_trigger("build_complete")
 
 # Whenever the BlockCache builds a block, update the Camera position.
-func _on_BlockCache_block_built(godot_path, real_path, local_pos, global_pos):
+func _CacheBuild_on_BlockCache_block_built(godot_path, real_path, local_pos, global_pos):
     $OrthoCamera.global_transform.origin = global_pos + ortho_shift
