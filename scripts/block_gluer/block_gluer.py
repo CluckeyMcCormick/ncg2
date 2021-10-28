05fa8a0fc37223a69b1d913d99ca9d7b2c537efb
frick-nedrickson <CluckeyMcCormick@github.com>
2021-10-27 17:52:14 -0700

    Process multiple maps via a function and arguments
    If we're going to glue multiple maps together, then we're going to
    need to process multiple maps. It's only natural!
    
    So, I moved the whole map-processing loop into a function. We now
    assume that all the arguments passed into the script are map paths.
    We need at least one, or we back out. Pass into the function to get
    entities, and then...
    
    Well, right now we just run a count of the different brushes and
    entities we have. Actually doing something is the NEXT step.

--- scripts/block_gluer/block_gluer.py
+++ scripts/block_gluer/block_gluer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import enum
+import sys
 
 class ProcessState(enum.IntEnum):
     OUTSIDE_ENTITY = 1
@@ -107,95 +108,113 @@ def face_dict_to_line(face_dict):
     
     return ret.format(**face_dict)
 
-state = ProcessState.OUTSIDE_ENTITY
-entity_list = []
+def file_to_entity_list(file_path):
+    state = ProcessState.OUTSIDE_ENTITY
+    entity_list = []
 
-current_entity = None
-current_brush = None
+    current_entity = None
+    current_brush = None
 
-f = open("../../blocks/trenchbroom_maps/s01_block07.map")
-for line in f:
-    # Okay - how we process this line depends on our current state. We'll use a
-    # series of if statements since Python has no switch statment.
-    
-    #
-    # Outside an entity
-    #
-    if state == ProcessState.OUTSIDE_ENTITY:
-        # If we've encountered a section-opener...
-    	if "{\n" == line:
-            # We've entered a new entity! Create an entity dictionary.
-            entity_list.append({})
-            # Create the entity's Brushes
-            entity_list[-1]["brushes"] = []
-            
-            # Point our 'current' entity at the entity we just made
-            current_entity = entity_list[-1]
-            
-            # Finally, mark that we're in an entity
-            state = ProcessState.INSIDE_ENTITY
-    
-    #
-    # Inside an entity
-    #
-    elif state == ProcessState.INSIDE_ENTITY:
-        # If we've encountered a section-opener...
-        if "{\n" == line:
-            # Then it has to be a brush! Add a brush to the current stack.
-            current_entity["brushes"].append([])
-            
-            # Point our 'current' brush at the brush we just made
-            current_brush = current_entity["brushes"][-1]
-            
-            # We're now inside a brush!
-            state = ProcessState.INSIDE_BRUSH
-            
-        # Otherwise, if we've encountered a section-closer...
-        elif "}\n" == line:
-            # Okay, then this entity is closing out. We're now outside the
-            # entity.
-            state = ProcessState.OUTSIDE_ENTITY
-            
-            # We don't have a current entity anymore
-            current_entity = None
-            
-        # Otherwise, if this line starts with a ", then this is an entity
-        # key/value
-        elif "\"" == line[0]:
-            # Split into two fragments, based on the gap between key and value
-            kv_list = line.split("\" \"")
-            # Remove any ancillary/remaining " characters from the key and/or
-            # value
-            ent_key = kv_list[0].replace("\"", "")
-            ent_val = kv_list[1].replace("\"", "")
-            
-            # So long as the ent key isn't our brushes, we can then comfortably
-            # stick it in 
-            if ent_key != "brushes":
-                current_entity[ent_key] = ent_val
-
-    #
-    # Inside a brush
-    #
-    elif state == ProcessState.INSIDE_BRUSH:
-        # If we've encountered a section-closer...
-        if "}\n" == line:
-            # Okay, then this brush is closing out. We're now inside the
-            # entity.
-            state = ProcessState.INSIDE_ENTITY
-            # We don't have a current brush anymore
-            current_brush = None
-        # Otherwise, so long as this line isn't a comment line...
-        elif line[:2] != "//":
-            # Then process the current line into a dictionary and then add it to
-            # the brush array
-            current_brush.append( dict_from_face_line(line) )
+    f = open(file_path)
+    for line in f:
+        # Okay - how we process this line depends on our current state. We'll
+        # use a series of if statements since Python has no switch statment.
         
-for entity in entity_list:
-    print("ENTITY")
-    for brush in entity["brushes"]:
-        print("\tBRUSH!")
-        for face in brush:
-            print("\t\t", face_dict_to_line(face))
-    
-    
+        #
+        # Outside an entity
+        #
+        if state == ProcessState.OUTSIDE_ENTITY:
+            # If we've encountered a section-opener...
+            if "{\n" == line:
+                # We've entered a new entity! Create an entity dictionary.
+                entity_list.append({})
+                # Create the entity's Brushes
+                entity_list[-1]["brushes"] = []
+                
+                # Point our 'current' entity at the entity we just made
+                current_entity = entity_list[-1]
+                
+                # Finally, mark that we're in an entity
+                state = ProcessState.INSIDE_ENTITY
+        
+        #
+        # Inside an entity
+        #
+        elif state == ProcessState.INSIDE_ENTITY:
+            # If we've encountered a section-opener...
+            if "{\n" == line:
+                # Then it has to be a brush! Add a brush to the current stack.
+                current_entity["brushes"].append([])
+                
+                # Point our 'current' brush at the brush we just made
+                current_brush = current_entity["brushes"][-1]
+                
+                # We're now inside a brush!
+                state = ProcessState.INSIDE_BRUSH
+                
+            # Otherwise, if we've encountered a section-closer...
+            elif "}\n" == line:
+                # Okay, then this entity is closing out. We're now outside the
+                # entity.
+                state = ProcessState.OUTSIDE_ENTITY
+                
+                # We don't have a current entity anymore
+                current_entity = None
+                
+            # Otherwise, if this line starts with a ", then this is an entity
+            # key/value
+            elif "\"" == line[0]:
+                # Split into two fragments, based on the gap between key and
+                # value
+                kv_list = line.split("\" \"")
+                # Remove any ancillary/remaining " characters from the key
+                # and/or value
+                ent_key = kv_list[0].replace("\"", "")
+                ent_val = kv_list[1].replace("\"", "")
+                
+                # So long as the ent key isn't our brushes, we can then
+                # comfortably stick it in 
+                if ent_key != "brushes":
+                    current_entity[ent_key] = ent_val
+
+        #
+        # Inside a brush
+        #
+        elif state == ProcessState.INSIDE_BRUSH:
+            # If we've encountered a section-closer...
+            if "}\n" == line:
+                # Okay, then this brush is closing out. We're now inside the
+                # entity.
+                state = ProcessState.INSIDE_ENTITY
+                # We don't have a current brush anymore
+                current_brush = None
+            # Otherwise, so long as this line isn't a comment line...
+            elif line[:2] != "//":
+                # Then process the current line into a dictionary and then add
+                # it to the brush array
+                current_brush.append( dict_from_face_line(line) )
+                
+    # Okay, throw back the entity list!
+    return entity_list
+
+#
+# SCRIPT START!
+#
+
+print("Received ", len(sys.argv), " arguments for processing.")
+
+if len(sys.argv) <= 1:
+    print("No actual maps received! Aborting!")
+    sys.exit()
+
+entity_count = 0
+brush_count = 0
+
+for map_path in sys.argv[1:]:
+    print(map_path)
+    entity_list = file_to_entity_list(map_path)
+    for entity in entity_list:
+        entity_count += 1
+        brush_count += len( entity["brushes"] )
+
+print("Found {:d} brushes across {:d} entities.".format(brush_count, entity_count))
