b62968d1d6bb7cd8c2aec805bf72a6ab8eb40172
frick-nedrickson <CluckeyMcCormick@github.com>
2021-11-02 18:59:21 -0700

    Set up func_group; track properties per-block
    If we're going to save time by mashing all of the Qodot blocks
    together into some sort of mushy Frankenstein, we're going to need a
    way to differentiate between all those different Frankenstein parts
    so we can easily pick out blocks and other features from the block
    cache.
    
    To do that, we needed to somehow group together the disparate point
    entities. Best way to do that? It'd actually be placing the nodes
    together in a tree structure, except Qodot doesn't do that. So,
    instead, we'll assign everything to a Godot group as it comes in.
    
    Thus, I modified the func_group to take in and store some extra
    properties as well as set it's group at run time. I did the same for
    the light entity/class as well. It seems to be working, for now. We'll
    need to rewrite how we copy things from the BlockCache, but this is
    definitely the best way forward right now.

--- addons/qodot/game_definitions/fgd/solid_classes/button.gd
+++ addons/qodot/game_definitions/fgd/solid_classes/button.gd
@@ -19,86 +19,86 @@ var release_signal_delay :=  0.0
 var overlaps := 0
 
 func set_properties(new_properties: Dictionary) -> void:
-	if properties != new_properties:
-		properties = new_properties
-		update_properties()
+    if properties != new_properties:
+        properties = new_properties
+        update_properties()
 
 func update_properties() -> void:
-	if 'axis' in properties:
-		axis = properties.axis.normalized()
+    if 'axis' in properties:
+        axis = properties.axis.normalized()
 
-	if 'speed' in properties:
-		speed = properties.depth
+    if 'speed' in properties:
+        speed = properties.depth
 
-	if 'depth' in properties:
-		depth = properties.depth.to_float()
+    if 'depth' in properties:
+        depth = properties.depth.to_float()
 
-	if 'release_delay' in properties:
-		release_delay = properties.release_delay
+    if 'release_delay' in properties:
+        release_delay = properties.release_delay
 
-	if 'trigger_signal_delay' in properties:
-		trigger_signal_delay = properties.trigger_signal_delay
+    if 'trigger_signal_delay' in properties:
+        trigger_signal_delay = properties.trigger_signal_delay
 
-	if 'press_signal_delay' in properties:
-		press_signal_delay = properties.press_signal_delay
+    if 'press_signal_delay' in properties:
+        press_signal_delay = properties.press_signal_delay
 
-	if 'release_signal_delay' in properties:
-		release_signal_delay = properties.release_signal_delay
+    if 'release_signal_delay' in properties:
+        release_signal_delay = properties.release_signal_delay
 
 func _init() -> void:
-	connect("body_shape_entered", self, "body_shape_entered")
-	connect("body_shape_exited", self, "body_shape_exited")
+    connect("body_shape_entered", self, "body_shape_entered")
+    connect("body_shape_exited", self, "body_shape_exited")
 
 func _enter_tree() -> void:
-	base_translation = translation
+    base_translation = translation
 
 func _process(delta: float) -> void:
-	var target_position = base_translation + (axis * (depth if pressed else 0.0))
-	translation = translation.linear_interpolate(target_position, speed * delta)
+    var target_position = base_translation + (axis * (depth if pressed else 0.0))
+    translation = translation.linear_interpolate(target_position, speed * delta)
 
 func body_shape_entered(body_id: int, body: Node, body_shape_idx: int, self_shape_idx: int) -> void:
-	if body is StaticBody:
-		return
+    if body is StaticBody:
+        return
 
-	if overlaps == 0:
-		press()
+    if overlaps == 0:
+        press()
 
-	overlaps += 1
+    overlaps += 1
 
 func body_shape_exited(body_id: int, body: Node, body_shape_idx: int, self_shape_idx: int) -> void:
-	if body is StaticBody:
-		return
+    if body is StaticBody:
+        return
 
-	overlaps -= 1
-	if overlaps == 0:
-		if release_delay == 0:
-			release()
-		elif release_delay > 0:
-			yield(get_tree().create_timer(release_delay), "timeout")
-			release()
+    overlaps -= 1
+    if overlaps == 0:
+        if release_delay == 0:
+            release()
+        elif release_delay > 0:
+            yield(get_tree().create_timer(release_delay), "timeout")
+            release()
 
 func press() -> void:
-	if pressed:
-		return
+    if pressed:
+        return
 
-	pressed = true
+    pressed = true
 
-	emit_trigger()
-	emit_pressed()
+    emit_trigger()
+    emit_pressed()
 
 func emit_trigger() -> void:
-	yield(get_tree().create_timer(trigger_signal_delay), "timeout")
-	emit_signal("trigger")
+    yield(get_tree().create_timer(trigger_signal_delay), "timeout")
+    emit_signal("trigger")
 
 func emit_pressed() -> void:
-	yield(get_tree().create_timer(press_signal_delay), "timeout")
-	emit_signal("pressed")
+    yield(get_tree().create_timer(press_signal_delay), "timeout")
+    emit_signal("pressed")
 
 func release() -> void:
-	if not pressed:
-		return
+    if not pressed:
+        return
 
-	pressed = false
+    pressed = false
 
-	yield(get_tree().create_timer(release_signal_delay), "timeout")
-	emit_signal("released")
+    yield(get_tree().create_timer(release_signal_delay), "timeout")
+    emit_signal("released")
