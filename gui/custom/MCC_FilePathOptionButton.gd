5ac3b93815e2c041684ec3f9896a6b87cebae1ef
frick-nedrickson <CluckeyMcCormick@github.com>
2022-08-15 22:32:17 -0700

    Create MaterialColorControl integrated widgets
    Okay, so I was trying to rework a GUI and it was driving me utterly
    insane. I was copying and pasting code, trying to coordinate nodes
    against keys. Eventually, I got exasperated and exclaimed, "God, I
    wish I could just tie these GUI widgets directly into the Material
    Color Control!". I sat back for a second before realizing that there
    was no reason I COULDN'T just do that.
    
    So, I've add a set of MaterialColorControl widgets that integrate
    directly with the MCC. you just give them a key to address against
    and they do the rest. It rules. I'm really happy with it. Now I just
    need to tear apart all the existing GUIs with my bare hands...

---
+++ gui/custom/MCC_FilePathOptionButton.gd
@@ -0,0 +1,97 @@
+extends OptionButton
+
+# Utility script to find resources
+const DIR_RES_FIND = preload("res://util/DirectoryResourceFinder.gd")
+
+# Grab the MaterialColorControl Node - this will allow us to change colors and
+# materials on the fly.
+onready var mcc = get_node("/root/MaterialColorControl")
+
+# What's the Material Color Control Key property we're targeting?
+export(String) var target_key
+
+# What's the default directory we're targeting?
+export(String) var default_directory
+# What's the extension we're looking for on files in the default directory?
+export(String) var default_extension
+
+# What's the user directory we're targeting?
+export(String) var user_directory
+# What's the extension we're looking for on files in the user directory?
+export(String) var user_extension
+
+# Do we update the global profile to reflect our values whenever a value gets
+# updated? We need this to stop ourselves from accidentally looping forever.
+var _update_global = true
+
+# These two dictionaries are used for getting info out of, and updating, the
+# texture selection GUI elements
+var id_to_path = {}
+var path_to_id = {}
+
+func _ready():
+    # What's the current file we're looking at?
+    var file_name
+    # What's the our current list of resources? Mind you this isn't necessarily
+    # a captial-R Resource - just a file path of some kind
+    var resources
+    # What's the current id for what we're putting in our texture selection? We
+    # need to assign these manually so we can easily get to them.
+    var id = 0
+    
+    # Connect to the key update signal, so we can respond to key changes.
+    mcc.connect("key_update", self, "_on_mcc_key_update")
+    # Connect to our own "item_selected" signal. We do this in code to ensure
+    # the user doesn't accidentally disconnect the signal when including this in
+    # a scene.
+    self.connect("item_selected", self, "_on_item_selected")
+    
+    # Get a list of the paths-to-be-found in the configured directory (with the
+    # appropriate extension, of course).
+    resources = DIR_RES_FIND.get_path_resources(
+        default_directory, default_extension
+    )
+
+    # Sort the textures so the names are alphabetical
+    resources.sort()
+    
+    # For each path...
+    for path in resources:
+        # Get the file name, captialized
+        file_name = path.get_file().get_basename().capitalize()
+        # Stick it in the selection areas
+        self.add_item(file_name, id)
+        # Track the id to the path and vice versa
+        id_to_path[id] = path
+        path_to_id[path] = id
+        # Increment the id
+        id += 1
+
+func _on_mcc_key_update(key):
+    # Disable updating the globabl dictionary - we don't want to update
+    # something while we're reading from it!
+    _update_global = false
+    
+    # The path as given by the key
+    var path
+    
+    # Update only if our key matches.
+    match key:
+        target_key:
+            path = mcc.profile_dict[key]
+            if path in path_to_id:
+                self.select( self.get_item_index( path_to_id[path] ) )
+            else:
+                print("Can't find item for ", path)
+
+    # Enable global updating again
+    _update_global = true
+
+func _on_item_selected(color):
+    # Get the path
+    var path = id_to_path[ self.get_selected_id() ]
+    
+    if _update_global:
+        mcc.profile_dict[target_key] = path
+        mcc.update_key(target_key)
+
---
+++ util/DirectoryResourceFinder.gd
@@ -0,0 +1,54 @@
+
+# Loads the possible resources from the in_path directory; but only those that
+# match the given extension.
+static func get_path_resources(in_path, extension):
+    # What are the paths to our different textures?
+    var resources = []
+    # What's the current file we're looking at?
+    var file_name
+    # What's the current id for what we're putting in our texture selection? We
+    # need to assign these manually so we can easily get to them.
+    var id = 0
+    
+    # Now we need to dynamically build up our list of blocks we can use. To do
+    # so, we're gonna take a look at the blocks directory and see what we can
+    # do.
+    var dir = Directory.new()
+    
+    # If the directory didn't open, then we can't really do anything at all.
+    # Ergo, we'll have to back out.
+    if dir.open(in_path) != OK:
+        print("Couldn't open ", in_path, "! Can't do anything!!!")
+        return resources
+    
+    # Start the directory-listing-processing thing.
+    dir.list_dir_begin(true)
+    
+    # Get the first file in the directory listing
+    file_name = dir.get_next()
+    # While we still have a file...
+    while file_name != "":
+        # If the current file is a directory...
+        if dir.current_is_dir():
+            # Skip this file!
+            file_name = dir.get_next()
+            continue
+        # If the current file is an import...
+        if ".import" in file_name.to_lower():
+            # Skip this file!
+            file_name = dir.get_next()
+            continue
+        # If the current file has the appropriate extension...
+        if extension in file_name.to_lower():
+            # Then add it to the choice list!
+            resources.append(in_path + file_name)
+        
+        # Now that we've checked the filename and done any necessary actions,
+        # get the next file name.
+        file_name = dir.get_next()
+    
+    # Finish off the directory listing processing... thing.
+    dir.list_dir_end()
+    
+    # Return the final resource listing
+    return resources
