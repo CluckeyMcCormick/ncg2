e278071695aa919960f2c92d7d5617b9ecd5df45
frick-nedrickson <CluckeyMcCormick@github.com>
2022-10-05 00:37:25 -0700

    Create the necessary user subdirectories
    We're gonna need to load and save files to very particular user
    subdirectories. To do that, we need to make sure those subdirectories
    exist. I figure we can either do that at load-and-save, or we can do
    that right when the program starts.
    
    It made more sense to me to just create those directories right at the
    beginning. Adds a certain amount of consistency, ya know? The one
    downside is it's not runtime-resilient. So, if the user goes in there
    and destroys the subdirectories, it can't recover until the program
    restarts. That might be a bad idea... but I'm programming a bit too
    late for good ideas, I think.
    
    Anyway, the new UserDirEnforcer class is the one who handles this. I
    just added it as an AutoLoad - easy! It's now the first thing that
    happens in the program.

---
+++ util/UserDirEnforcer.gd
@@ -0,0 +1,41 @@
+extends Node
+
+# This node ensures the user:// directory has the appropriate subdirectories, so
+# we can save and load custom content at will.
+
+# What directories are we trying to create? These are all going to be under the
+# user:// directory/path.
+const DIRS = [
+    "profiles", "mods", "textures/antennae", "textures/particle",
+    "textures/moon", "textures/windows"
+]
+
+# Function called when this node enters the scene.
+func _ready():
+    # Result value; should be an Error type
+    var result
+    
+    # Create a new directory object
+    var dir = Directory.new()
+    
+    # Open the user directory.
+    dir.open("user://")
+    
+    # For each subdirectory...
+    for subdir in DIRS:
+        # If the subdirectory exists...
+        if dir.dir_exists(subdir):
+            continue
+        
+        # If we're here, this subdirectory doesn't exist - time to make it!
+        result = dir.make_dir_recursive(subdir)
+        
+        # If we got an error, tell the user
+        match result:
+            OK:
+                continue
+            _:
+                printerr(
+                    "Couldn't make subdirectory ", subdir,
+                    "! Error code ", result
+                )
